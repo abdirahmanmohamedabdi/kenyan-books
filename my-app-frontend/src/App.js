57f79097d825a2a2619f4d1cff70d20beb3b323e
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-09-11 21:04:53 +0300

    added routes to app.js

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,15 +1,32 @@
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from "./Components/Navbar"
+import React from "react"
+import Nav from "./Navbar"
+import Books from "./Components/Books";
+import "./styles.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
+
+
 function App() {
   return (
-    <div className="Nav">
-      <header>
+    <Router>
+      <div className="App">
         <Nav />
-      </header>
-      
-    </div>
-  );
+        <Routes>
+          
+        
+        <Route path="/books" exact element={<Books/>} />
+         
+    
+        </Routes>
+      </div>
+    </Router>
+    
+  )
 }
 
-export default App;
+export default App
